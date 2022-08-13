2a749e0e2d8ae61b80b078e831aff587dc9e4f62
Miguel Blanque <miguelblanque@gmail.com>
2022-08-13 12:20:56 +0200

    Mostrar Resumen y finalizacion de proyecto

--- README.md
+++ README.md
@@ -1,7 +1,19 @@
 # cotizador
+Proyecto cotizador de seguro de automovil con Context API 
 
 
-Proyecto cotizador de seguro de automovil con Context API
+
+Pagina principal de la aplicacion
+
+![alt text](/src/img/Principal.png)
+
+
+Cotizacion de un Auto
+
+![alt text](/src/img/Cotizacion.png)
+
+
+Ejecucion:
 
 npm create vite@latest
 
@@ -12,6 +24,7 @@ npm run dev
 
 npm i tailwindcss postcss autoprefixer
 npx tailwindcss init -p
+
 
 
 
--- src/components/Resultado.jsx
+++ src/components/Resultado.jsx
@@ -1,11 +1,48 @@
+import { useCallback, useMemo , useRef} from "react"
+
 import useCotizador from "../hooks/useCotizador"
+import { MARCAS, PLANES} from "../context"
 
 const Resultado = () => {
-        const { resultado } = useCotizador()
-    
+        const { resultado , datos } = useCotizador()
+        const { marca, plan, year } = datos
+
+       // Usamos useCallback para que no realice le re-render de la marca y plan  has que no se modifique el resultado
+        //Obtenemos el nombre de la marca. 
+        const [nombreMarca] = useCallback(MARCAS.filter(m => m.id=== Number(marca)), [resultado])
+
+         //Obtenemos el nombre del plan
+         const [nombrePlan] = useMemo( () => PLANES.filter(p => p.id=== Number(plan)), [resultado])
+         
+         //
+        const yearRef= useRef(year)
+
+    //Si no hay datos no se muestra nada
     if (resultado === 0 ) return null
+    //Mostramos el Resumen
     return (
-    <div>{resultado}</div>
+    <div className="bg-gray-100 text-center mt-5 p-5 shadow">
+      <h2 className="text-gray-600 font-black text-3xl">
+            Resumen
+      </h2>
+      <p className="my-2">
+        <span className="font-bold">Marca: </span>
+        {nombreMarca.nombre}
+      </p>
+      <p className="my-2">
+        <span className="font-bold">Plan: </span>
+        {nombrePlan.nombre}
+      </p>
+      <p className="my-2">
+        <span className="font-bold">Año: </span>
+        {yearRef.current}
+      </p>
+      <p className="my-2 text-2xl">
+        <span className="font-bold">Total Cotizacion: </span>
+        {resultado}
+      </p>
+
+    </div>
   )
 }
 
